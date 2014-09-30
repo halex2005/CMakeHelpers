50f4093c16060dc6ce1322f977df197dc82083f4
halex2005 <halex2005@e1.ru>
2014-09-30 09:55:26 +0600

    reimplement find pjsip for windows

--- FindPJSIP.cmake
+++ FindPJSIP.cmake
@@ -1,65 +1,3 @@
-#==============================================================
-# Helper functions
-#==============================================================
-# Guesses CppRest's compiler prefix used in built library names
-# Returns the guess by setting the variable pointed to by _ret
-function(_PJSIP_GUESS_COMPILER_PREFIX _ret)
-  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel"
-      OR "${CMAKE_CXX_COMPILER}" MATCHES "icl"
-      OR "${CMAKE_CXX_COMPILER}" MATCHES "icpc")
-    if(WIN32)
-      set (_PJSIP_COMPILER "-iw")
-    else()
-      set (_PJSIP_COMPILER "-il")
-    endif()
-  elseif (MSVC12)
-    set(_PJSIP_COMPILER "120")
-  elseif (MSVC11)
-    set(_PJSIP_COMPILER "110")
-  elseif (MSVC10)
-    set(_PJSIP_COMPILER "100")
-  elseif (MSVC90)
-    set(_PJSIP_COMPILER "90")
-  elseif (MSVC80)
-    set(_PJSIP_COMPILER "80")
-  elseif (MSVC71)
-    set(_PJSIP_COMPILER "71")
-  elseif (MSVC70) # Good luck!
-    set(_PJSIP_COMPILER "7") # yes, this is correct
-  elseif (MSVC60) # Good luck!
-    set(_PJSIP_COMPILER "6") # yes, this is correct
-  elseif (BORLAND)
-    set(_PJSIP_COMPILER "-bcb")
-  elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "SunPro")
-    set(_PJSIP_COMPILER "-sw")
-  elseif (MINGW)
-    _PJSIP_COMPILER_DUMPVERSION(_PJSIP_COMPILER_VERSION)
-    set(_PJSIP_COMPILER "-mgw${_PJSIP_COMPILER_VERSION}")
-  elseif (UNIX)
-    set(_PJSIP_COMPILER "")
-  else()
-    # TODO at least PJSIP_DEBUG here?
-    set(_PJSIP_COMPILER "")
-  endif()
-  set(${_ret} ${_PJSIP_COMPILER} PARENT_SCOPE)
-endfunction()
-
-#-------------------------------------------------------------------------------
-#
-# Runs compiler with "-dumpversion" and parses major/minor
-# version with a regex.
-#
-function(_PJSIP_COMPILER_DUMPVERSION _OUTPUT_VERSION)
-  exec_program(${CMAKE_CXX_COMPILER}
-    ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion
-    OUTPUT_VARIABLE _PJSIP_COMPILER_VERSION
-  )
-  string(REGEX REPLACE "([0-9])\\.([0-9])(\\.[0-9])?" "\\1\\2"
-    _PJSIP_COMPILER_VERSION ${_PJSIP_COMPILER_VERSION})
-  set(${_OUTPUT_VERSION} ${_PJSIP_COMPILER_VERSION} PARENT_SCOPE)
-endfunction()
-
-
 #-------------------------------------------------------------------------------
 #
 # Setup environment
@@ -78,46 +16,68 @@ mark_as_advanced(PJSIP_DIR)
 set(PJSIP_ROOT_DIR $ENV{PJSIP_DIR})
 
 if(WIN32)
-    set (PJSIP_INCLUDE_DIR ${PJSIP_ROOT_DIR})
-    set (PJSIP_LIBRARY_DIR ${PJSIP_ROOT_DIR}/lib)
-    set (PJSIP_LIBRARY_DIRS ${PJSIP_LIBRARY_DIR})
-
-    _PJSIP_GUESS_COMPILER_PREFIX(PJSIP_COMPILER)
-
-    find_path(PJSIP_PJLIB_INCLUDE_DIR      NAMES "pjlib.h"      PATHS "${PJSIP_ROOT_DIR}/pjlib/include")
-    find_path(PJSIP_PJLIB_UTIL_INCLUDE_DIR NAMES "pjlib-util.h" PATHS "${PJSIP_ROOT_DIR}/pjlib-util/include")
-    find_path(PJSIP_PJMEDIA_INCLUDE_DIR    NAMES "pjmedia.h"    PATHS "${PJSIP_ROOT_DIR}/pjmedia/include")
-    find_path(PJSIP_PJNATH_INCLUDE_DIR     NAMES "pjnath.h"     PATHS "${PJSIP_ROOT_DIR}/pjnath/include")
-    find_path(PJSIP_PJSIP_INCLUDE_DIR      NAMES "pjsip.h"      PATHS "${PJSIP_ROOT_DIR}/pjsip/include")
-
-    set (PJSIP_INCLUDE_DIR
-        ${PJSIP_PJLIB_INCLUDE_DIR}
-        ${PJSIP_PJLIB_UTIL_INCLUDE_DIR}
-        ${PJSIP_PJMEDIA_INCLUDE_DIR}
-        ${PJSIP_PJNATH_INCLUDE_DIR}
-        ${PJSIP_PJSIP_INCLUDE_DIR}
-    )
-    set (PJSIP_INCLUDE_DIRS ${PJSIP_INCLUDE_DIR})
-
-    if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
-        set(PJSIP_Platform_Suffix "x86_64-x64-vc${PJSIP_COMPILER}")
-    else()
-        set(PJSIP_Platform_Suffix "i386-Win32-vc${PJSIP_COMPILER}")
-    endif()
-
-    set(PJSIP_Platform_Suffix "${PJSIP_Platform_Suffix}-${CMAKE_CFG_INTDIR}")
-
+    set(PJSIP_VERSION 2.3.0)
     if (PJSIP_USE_STATIC_RUNTIME)
-        set(PJSIP_Platform_Suffix "${PJSIP_Platform_Suffix}-Static")
+        set(PJSIP_CRT_LINKAGE Static)
     else()
-        set(PJSIP_Platform_Suffix "${PJSIP_Platform_Suffix}-Dynamic")
+        set(PJSIP_CRT_LINKAGE Dynamic)
     endif()
 
-    set (PJSIP_LIBRARIES libpjproject-${PJSIP_Platform_Suffix})
-    if ("${PJSIP_INCLUDE_DIRS}" MATCHES "NOTFOUND" OR "${PJSIP_LIBRARY_DIRS}" MATCHES "NOTFOUND")
-        set(PJSIP_FOUND 0)
+    find_package(PkgConfig)
+    if (PkgConfig_FOUND)
+        if (NOT ${PKG_CONFIG_PATH} STREQUAL "" AND IS_DIRECTORY ${PKG_CONFIG_PATH})
+            # environment variable PKG_CONFIG_PATH exists and it is directory
+            if (NOT EXISTS ${PKG_CONFIG_PATH}/libpjproject.pc)
+                configure_file(${CMAKE_CURRENT_LIST_DIR}/libpjproject.in ${PKG}/libpjproject.pc)
+            endif()
+        else()
+            # there is no PKG_CONFIG_PATH environment variable
+        endif()
+        if (PJSIP_FIND_VERSION)
+            if (PJSIP_FIND_VERSION_EXACT)
+                pkg_check_modules(PJSIP libpjproject=${PJSIP_FIND_VERSION} REQUIRED)
+            else()
+                pkg_check_modules(PJSIP libpjproject>=${PJSIP_FIND_VERSION} REQUIRED)
+            endif()
+        else()
+            pkg_check_modules(PJSIP libpjproject REQUIRED)
+        endif()
     else()
-        set (PJSIP_FOUND 1)
+        if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+            set(PJSIP_TargetCPU "x86_64")
+        else()
+            set(PJSIP_TargetCPU "i386")
+        endif()
+
+        find_path(PJSIP_PJLIB_INCLUDE_DIR      NAMES "pjlib.h"      PATHS "${PJSIP_ROOT_DIR}/pjlib/include")
+        find_path(PJSIP_PJLIB_UTIL_INCLUDE_DIR NAMES "pjlib-util.h" PATHS "${PJSIP_ROOT_DIR}/pjlib-util/include")
+        find_path(PJSIP_PJMEDIA_INCLUDE_DIR    NAMES "pjmedia.h"    PATHS "${PJSIP_ROOT_DIR}/pjmedia/include")
+        find_path(PJSIP_PJNATH_INCLUDE_DIR     NAMES "pjnath.h"     PATHS "${PJSIP_ROOT_DIR}/pjnath/include")
+        find_path(PJSIP_PJSIP_INCLUDE_DIR      NAMES "pjsip.h"      PATHS "${PJSIP_ROOT_DIR}/pjsip/include")
+
+        set (PJSIP_INCLUDE_DIR
+            ${PJSIP_PJLIB_INCLUDE_DIR}
+            ${PJSIP_PJLIB_UTIL_INCLUDE_DIR}
+            ${PJSIP_PJMEDIA_INCLUDE_DIR}
+            ${PJSIP_PJNATH_INCLUDE_DIR}
+            ${PJSIP_PJSIP_INCLUDE_DIR}
+        )
+
+        set (PJSIP_LIBRARY_DIR ${PJSIP_ROOT_DIR}/lib)
+        if (PJSIP_USE_STATIC_RUNTIME)
+            set (PJSIP_LIBRARIES libpjproject-$(Platform)-$(PlatformToolset)-$(Configuration)-Static.lib)
+        else
+            set (PJSIP_LIBRARIES libpjproject-$(Platform)-$(PlatformToolset)-$(Configuration)-Dynamic.lib)
+        endif()
+
+        set (PJSIP_INCLUDE_DIRS ${PJSIP_INCLUDE_DIR})
+        set (PJSIP_LIBRARY_DIRS ${PJSIP_LIBRARY_DIR})
+
+        if ("${PJSIP_INCLUDE_DIRS}" MATCHES "NOTFOUND" OR "${PJSIP_LIBRARY_DIRS}" MATCHES "NOTFOUND")
+            set(PJSIP_FOUND 0)
+        else()
+            set (PJSIP_FOUND 1)
+        endif()
     endif()
 elseif(UNIX)
     find_package(PkgConfig REQUIRED)
@@ -132,11 +92,11 @@ elseif(UNIX)
     endif()
 endif()
 
-
 if (PJSIP_DEBUG)
     message(STATUS "PJSIP_ROOT_DIR     = ${PJSIP_ROOT_DIR}")
     message(STATUS "PJSIP_FOUND        = ${PJSIP_FOUND}")
     message(STATUS "PJSIP_INCLUDE_DIRS = ${PJSIP_INCLUDE_DIRS}")
     message(STATUS "PJSIP_LIBRARY_DIRS = ${PJSIP_LIBRARY_DIRS}")
     message(STATUS "PJSIP_LIBRARIES    = ${PJSIP_LIBRARIES}")
+    message(STATUS "PJSIP_VERSION      = ${PJSIP_VERSION}")
 endif()
